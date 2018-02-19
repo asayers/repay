8aca1b4f9691a90db3f5d1fefb94681d54e6707e
Alex Sayers <alex.sayers@gmail.com>
2018-02-19 20:32:30 +0900

    Some tune-ups

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,36 @@
 extern crate clap;
 extern crate env_logger;
-extern crate itertools;
 #[macro_use] extern crate log;
 extern crate mcmf;
 extern crate serde;
 #[macro_use] extern crate serde_derive;
 extern crate serde_json;
 
-use itertools::Itertools;
 use mcmf::*;
 use std::collections::BTreeMap;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
 
 fn main() {
     // Parse the command-line arguments
     let opts = clap::App::new("debtor").version("1.0")
         .args_from_usage(
             "<PATH>         'The ledger containing historical transactions'
-             -q --quiet     'Don't print anything unless there's a problem'
              -v...          'Increase the level of verbosity'")
         .get_matches();
 
     // Initialise the logger (prints to stderr)
     let log_level = match opts.occurrences_of("v") {
-        _ if opts.is_present("q") => log::LevelFilter::Error,
-        0 => log::LevelFilter::Info,
-        1 => log::LevelFilter::Debug,
-        2 | _ => log::LevelFilter::Trace,
+        0 => log::LevelFilter::Warn,
+        1 => log::LevelFilter::Info,
+        2 => log::LevelFilter::Debug,
+        3 | _ => log::LevelFilter::Trace,
     };
     env_logger::Builder::new().filter(None, log_level).init();
 
     let ledger_path = opts.value_of("PATH").unwrap();
     let ledger_file = File::open(ledger_path).unwrap();
-    let ledger_iter = BufReader::new(ledger_file)
-        .lines()
-        .map(|l| serde_json::from_str::<Transfer>(&l.unwrap()).unwrap());
+    let ledger_iter = serde_json::Deserializer::from_reader(ledger_file)
+        .into_iter().map(|x| x.expect("Deserialise line"));
 
     for p in compute_repayments(ledger_iter) {
         println!("{}", serde_json::to_string(&p).unwrap());
@@ -63,15 +58,17 @@ fn compute_repayments<I: IntoIterator<Item = Transfer>>(ledger: I) -> Vec<Transf
         *to += transfer.amt as isize;
         n += 1;
     }
-    info!("Read {} entries.", n);
+    info!("Done! Read {} entries", n);
+    debug!("{:?}", balances);
 
     // (Step 1.5: Set up a fully-connected graph with one node per person)
     info!("Setting up graph...");
     let mut graph = GraphBuilder::new();
-    let people = balances.keys();
-    for (x, y) in people.clone().cartesian_product(people) {   // unnecessary clone
-        if x != y {
-            graph.add_edge(x.clone(), y.clone(), Capacity(1_000_000), Cost(1));
+    for x in balances.keys() {
+        for y in balances.keys() {
+            if x != y {
+                graph.add_edge(x.clone(), y.clone(), Capacity(1_000_000_000), Cost(1));
+            }
         }
     }
 
@@ -86,13 +83,16 @@ fn compute_repayments<I: IntoIterator<Item = Transfer>>(ledger: I) -> Vec<Transf
         }
     }
     let (cost, paths) = graph.mcmf();
-    info!("Total repayable: {}", cost);
+    info!("Done! Total repayable: {}", cost);
 
     // (Step 2.5: Wrangle these flows back into the shape of Tranfers)
     info!("Assembling repayments...");
     let mut repayments = vec![];
     for mut p in paths {
-        // let msg = "Graph is strongly connected => all flows are length 1";
+        if p.flows.len() != 3 {
+            // Graph is strongly connected => all flows should have length 1
+            warn!("Maximum transfer amount exceeded.  Repaying via a different route...");
+        }
         for Flow { a, b, amount, .. } in p.flows {
             if let Vertex::Node(a) = a {
                 if let Vertex::Node(b) = b {
