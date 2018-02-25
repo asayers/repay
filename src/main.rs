b670b032d391888b331d4aff257f2aad1d3c55ed
Alex Sayers <alex.sayers@gmail.com>
2018-02-26 01:30:16 +0900

    Initial implementation of exact mode

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,14 @@ mod stack;
 use mcmf::*;
 use std::collections::BTreeMap;
 use std::fs::File;
+use stack::Stack;
 
 fn main() {
     // Parse the command-line arguments
     let opts = clap::App::new("debtor").version("1.0")
         .args_from_usage(
             "<PATH>         'The ledger containing historical transactions'
+             -x             'Guarantee an exact solution (may be slow)'
              -v...          'Increase the level of verbosity'")
         .get_matches();
 
@@ -34,8 +36,14 @@ fn main() {
     let ledger_iter = serde_json::Deserializer::from_reader(ledger_file)
         .into_iter().map(|x| x.expect("Deserialise line"));
 
-    for p in compute_repayments(ledger_iter) {
-        println!("{}", serde_json::to_string(&p).unwrap());
+    if opts.is_present("x") {
+        for p in compute_repayments_exact(ledger_iter) {
+            println!("{}", serde_json::to_string(&p).unwrap());
+        }
+    } else {
+        for p in compute_repayments_approx(ledger_iter) {
+            println!("{}", serde_json::to_string(&p).unwrap());
+        }
     }
 }
 
@@ -46,7 +54,92 @@ struct Transfer {
     amt: usize,
 }
 
-fn compute_repayments<I: IntoIterator<Item = Transfer>>(ledger: I) -> Vec<Transfer> {
+fn compute_repayments_exact<I: IntoIterator<Item = Transfer>>(ledger: I) -> Vec<Transfer> {
+    // Step 1: Compute everyone's balances (starting from 0)
+    info!("Reading ledger...");
+    let mut n = 0;
+    let mut balances = BTreeMap::new();
+    for transfer in ledger {
+        {
+        let from = balances.entry(transfer.from).or_insert(0);
+        *from -= transfer.amt as isize;
+        }
+        let to = balances.entry(transfer.to).or_insert(0);
+        *to += transfer.amt as isize;
+        n += 1;
+    }
+    info!("Done! Read {} entries", n);
+    debug!("{:?}", balances);
+
+    // Step 2: Order people from smallest to largest absolute balance
+    // TODO: Use a priority search queue in step 1, ie. a heap which lets you modify priorities.
+    let mut balances: Vec<(String, isize)> = balances.into_iter().collect();
+    balances.sort_unstable_by_key(|&(_, x)| -x.abs());
+
+    let mut best = (usize::max_value(), Stack::new());
+    let mut balance_refs: Vec<(&str, isize)> = Vec::new();
+    for &(ref s, x) in balances.iter() {
+        balance_refs.push((&s, x));
+    }
+    search_tree(&mut best, Stack::new(), balance_refs);
+
+    let mut ret = vec![];
+    for rc in best.1.iter() {
+        let edge = rc.1;
+        if edge.2 > 0 {
+            ret.push(Transfer {
+                from: edge.0.to_owned(),
+                to: edge.1.to_owned(),
+                amt: edge.2 as usize,
+            });
+        } else {
+            ret.push(Transfer {
+                from: edge.1.to_owned(),
+                to: edge.0.to_owned(),
+                amt: -edge.2 as usize,
+            });
+        }
+    }
+    ret
+}
+
+fn search_tree<'a, 'b>(best: &'b mut (usize, Stack<(&'a str, &'a str, isize)>), stack: Stack<(&'a str, &'a str, isize)>, mut remaining: Vec<(&'a str, isize)>) {
+    match remaining.pop() {
+        None => {
+            debug!("LEAF!");
+            if stack.len() < best.0 {
+                debug!("it's good!");
+                *best = (stack.len(), stack);
+            }
+        }
+        Some(head) => {
+            debug!(">> {}: smallest node: {:?}, ({:?})", stack.len(), head, remaining);
+            let mut matches = false;
+            for (x, i) in remaining.iter().zip(0..) {
+                debug!("try eliminating with {:?}?", x);
+                if x.1.signum() == head.1.signum() { continue; }
+                if x.1.abs() < head.1.abs() { continue; }
+                matches = true;
+                let mut next = remaining.clone();
+                next[i].1 += head.1;
+                if next[i].1 == 0 {
+                    next.remove(i);
+                }
+                next.sort_unstable_by_key(|&(_, x)| -x.abs());
+                search_tree(best, stack.push((head.0, x.0, head.1)), next)
+            }
+            if !matches {
+                debug!("LEAF!");
+                if stack.len() < best.0 {
+                    debug!("it's good!");
+                    *best = (stack.len(), stack);
+                }
+            }
+        }
+    }
+}
+
+fn compute_repayments_approx<I: IntoIterator<Item = Transfer>>(ledger: I) -> Vec<Transfer> {
     // Step 1: Compute everyone's balances (starting from 0)
     info!("Reading ledger...");
     let mut n = 0;
